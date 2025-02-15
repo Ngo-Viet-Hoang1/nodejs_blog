541bdc37dc9ca1ade15e6aa95870cc7400c7dcb1
Ngô Việt Hoàng <162393499+Ngo-Viet-Hoang1@users.noreply.github.com>
2025-02-15 15:22:49 +0700

    query parameters, form default behaviour, post method use middleware

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded({
+    extended: true,
+}))
+app.use(express.json())
+
 app.use(morgan('combined'))
 
 // Template engine
@@ -21,7 +26,19 @@ app.get('/', (req, res) => {
 })
 
 app.get('/news', (req, res) => {
+    console.log(req.query);
     res.render('news')
 })
+
+app.get('/search', (req, res) => {
+    console.log(req.query);
+    res.render('search')
+})
+
+app.post('/search', (req, res) => {
+    console.log(req.body);
+    res.render('search')
+})
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port} on port ${port}`))
